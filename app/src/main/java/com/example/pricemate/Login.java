be7d49cbc2056b30b757ab98db5e1c10ef74a042
19727 <benjamin.kim2121@gmail.com>
2020-10-23 23:15:18 -0700

    added Firebase Auth functionality. Create a new account. If already signed in, signout to create new account. Text ben if it does not work.

--- app/src/main/java/com/example/pricemate/Login.java
+++ app/src/main/java/com/example/pricemate/Login.java
@@ -1,5 +1,6 @@
 package com.example.pricemate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,9 +8,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+//Will move this from Login Activity to Main Activity::10/22
 public class Login extends AppCompatActivity {
     Button logIn, signUp;
-
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,13 +31,14 @@ public class Login extends AppCompatActivity {
         logIn = findViewById(R.id.login);
         signUp = findViewById(R.id.signup);
 
-        logIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                finish();
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
+//        logIn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+//                finish();
+//            }
+//        });
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,4 +47,61 @@ public class Login extends AppCompatActivity {
             }
         });
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //there is a login loop because it happens whenever it starts but never leaves, learn more
+        //about Android life cycle to break this.
+        if(FirebaseAuth.getInstance().getCurrentUser() == null){
+            createSignInIntent();
+        }
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+    private void createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers, Facebook and Twitter and special additional requirements
+        // in gradle so don't forget to add
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.AnonymousBuilder().build());
+        Intent intent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.pricemate)
+                .build();
+
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    public void signOut(View view){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                        createSignInIntent();
+                    }
+                });
+    }
+
+
 }
--- app/src/main/java/com/example/pricemate/MainActivity.java
+++ app/src/main/java/com/example/pricemate/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.pricemate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     Button buy, buy1;
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
 
         buy = findViewById(R.id.buy);
         buy1 = findViewById(R.id.buy2);
@@ -28,5 +41,42 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,"Item Bought",Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //there is a login loop because it happens whenever it starts but never leaves, learn more
+        //about Android life cycle to break this.
+        if(FirebaseAuth.getInstance().getCurrentUser() == null){
+            createSignInIntent();
+        }
+
+    }
+    private void createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers, Facebook and Twitter and special additional requirements
+        // in gradle so don't forget to add
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.AnonymousBuilder().build());
+        Intent intent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.pricemate)
+                .build();
+
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    public void signOut(View view){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                        createSignInIntent();
+                    }
+                });
     }
 }
