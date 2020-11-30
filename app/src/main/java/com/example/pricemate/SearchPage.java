6176dd013b9906054e9bbf21aa5a833d83b56d9b
19727 <benjamin.kim2121@gmail.com>
2020-11-30 15:44:04 -0800

    Removed Unused Activities

--- app/src/main/java/com/example/pricemate/SearchPage.java
+++ app/src/main/java/com/example/pricemate/SearchPage.java
@@ -1,10 +1,12 @@
 package com.example.pricemate;
 
+
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 
-public class SearchPage extends WearableActivity {
+
+public class SearchPage extends WearableActivity{
 
     private TextView mTextView;
 
